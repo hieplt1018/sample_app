f4d0d30129c59fb446c11f1be1ec79ba9f46e291
hieplt1018 <hieplt1018@gmail.com>
2019-07-08 07:47:42 +0700

    Chapter10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find_by params[:id]
-    return if @user
-    flash[:error] = t("error_page.error_message")
-    redirect_to root_path
+  before_action :logged_in_user, except: %i(show new create)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: %i(destroy)
+  before_action :load_user, except: %i(index new create)
+
+  def index
+    @users = User.order(:name).page(params[:page]).per(Settings.accountPerPage)
   end
+
+  def show; end
 
   def new
     @user = User.new
@@ -20,9 +24,52 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t "error_page.edit_success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "error_page.delete_success"
+    else
+      flash[:danger] = t "error_page.delete_fail"
+    end
+    redirect_to users_url
+  end
+
   private
     def user_params
       params.require(:user).permit :name, :email, :password,
         :password_confirmation
+    end
+
+    def logged_in_user
+      return if logged_in?
+      store_location
+      flash[:danger] = t "error_page.edit_danger"
+      redirect_to login_url
+    end
+
+    def correct_user
+      redirect_to root_url unless current_user? @user
+    end
+
+    def admin_user
+      redirect_to root_url unless current_user.admin?
+    end
+
+    def load_user
+      @user = User.find_by id: params[:id]
+      return if @user
+      flash[:error] = t("error_page.error_message")
+      redirect_to root_path
     end
 end
